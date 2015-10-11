9c06c714c033b404ac1322009e8a47fe354ec1e7
constXife <constXife@yandex.ru>
2015-10-12 03:44:49 +0700

    Modify deploy script.

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,7 +11,7 @@ set :repo_url, 'git@github.com:constXife/blog.git'
 set :deploy_to, '/home/constxife/blog'
 
 # Default value for :linked_files is []
-# set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
+set :linked_files, fetch(:linked_files, []).push('config/puma.rb')
 
 # Default value for linked_dirs is []
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle',
@@ -20,7 +20,10 @@ set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', '
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 
-set :puma_conf,   -> { File.join(shared_path, 'config/puma.rb') }
+set :puma_conf,  "#{shared_path}/config/puma.rb"
+set :puma_state, "#{shared_path}/tmp/pids/blog.state"
+set :puma_pid, "#{shared_path}/tmp/pids/blog.pid"
+set :puma_bind, "unix://#{shared_path}/tmp/sockets/blog.sock"
 
 set :keep_releases, 1
 
